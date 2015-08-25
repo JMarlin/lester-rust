10617fbcb9b53a5a733ca32494ea7ad3182dc7d4
jmarlin <j.marlin@outlook.com>
2015-08-25 15:54:03 -0400

    Added type conversion code

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,10 @@ fn main() {
 		
 	println!("You guessed: {}", guess);
 	
+	let guess: u32 = guess.trim().parse()
+		.ok()
+		.expect("Please type a number!");
+	
 	match guess.cmp(&secret_number) {
 		Ordering::Less    => println!("Too small!"),
 		Ordering::Greater => println!("Too big!"),
