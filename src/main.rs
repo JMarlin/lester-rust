1b8e125fa65029aa87cd89b6134cba57cbd78859
jmarlin <j.marlin@outlook.com>
2015-08-25 16:10:44 -0400

    Began dining philosophers tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,22 @@
-extern crate rand;
+struct Philosopher {
+	name: String
+}
 
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+impl Philosopher {
+
+	fn new(name: &str) -> Philosopher {
+		
+		Philosopher {
+			name: name.to_string()
+		}
+	}
+}
 
 fn main() {
-	
-	println!("Guess the number!");
-	let secret_number = rand::thread_rng().gen_range(1, 101);
-	
-	loop {
-	
-		println!("Please input your guess: ");
-		let mut guess = String::new();
-		
-		io::stdin().read_line(&mut guess)
-			.ok()
-			.expect("Failed to read line");
-			
-		let guess: u32 = match guess.trim().parse() {
-		
-			Ok(num) => num,
-			Err(_)  => continue
-		};
-		
-		println!("You guessed: {}", guess);
-		
-		match guess.cmp(&secret_number) {
-		
-			Ordering::Less    => println!("Too small!"),
-			Ordering::Greater => println!("Too big!"),
-			Ordering::Equal   => {
-				
-				println!("You win!");
-				break;
-			}
-		}
-	} 
+
+	let p1 = Philosopher::new("Judith Butler");
+	let p2 = Philosopher::new("Gilles Deleuze");
+	let p3 = Philosopher::new("Karl Marx");
+	let p4 = Philosopher::new("Emma Goldman");
+	let p5 = Philosopher::new("Michel Foucault");
 }
