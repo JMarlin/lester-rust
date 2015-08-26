96dbc860f3dbc40d454ebe6009d0a8a73b508989
jmarlin <j.marlin@outlook.com>
2015-08-26 12:10:41 -0400

    Fixed period-should-be-comma typo

--- test.rb
+++ test.rb
@@ -3,7 +3,7 @@ require 'ffi'
 module Hello
 	extend FFI::Library
 	ffi_lib 'target/release/libembed.so'
-	attach_function :process, []. :void
+	attach_function :process, [], :void
 end
 
 Hello.process
