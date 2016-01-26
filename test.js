c2159d19529719f5493c1032e6962e6f4a66a459
Joseph Marlin <joe@thinkbeast.local>
2016-01-25 22:17:39 -0500

    Created a cargo project

--- test.js
+++
@@ -1,9 +0,0 @@
-var ffi = require('ffi');
-
-var lib = ffi.Library('target/release/liblester', {
-		'process': ['void', []]
-	});
-	
-lib.process();
-
-console.log("done!");
--- test.py
+++
@@ -1,7 +0,0 @@
-from ctypes import cdll
-
-lib = cdll.LoadLibrary("target/release/liblester.so")
-
-lib.process()
-
-print("done!")
